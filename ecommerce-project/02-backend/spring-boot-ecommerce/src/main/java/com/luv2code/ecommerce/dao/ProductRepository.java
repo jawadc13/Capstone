2c60e3f1a82b08f19d5a7bc0e2154f8cc2f3b63c
Jawad Chowdhury <jawadc13@gmail.com>
2022-03-03 14:25:18 -0600

    Adding Springboot backend with Angular frontend in ecommerce project

---
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.luv2code.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.luv2code.ecommerce.entity.Product;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
