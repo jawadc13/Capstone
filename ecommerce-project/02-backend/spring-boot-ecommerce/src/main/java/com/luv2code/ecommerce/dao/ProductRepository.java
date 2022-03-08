4c5554b77d0b04c3af44fffe69093783fe4860e0
Jawad Chowdhury <jawadc13@gmail.com>
2022-03-08 13:56:33 -0600

    Adding ecommerce front and backend up to section 21 (this file is big :0  )

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.luv2code.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.ecommerce.entity.Product;
 
@@ -10,4 +13,7 @@ import com.luv2code.ecommerce.entity.Product;
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pagable);
+	
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
